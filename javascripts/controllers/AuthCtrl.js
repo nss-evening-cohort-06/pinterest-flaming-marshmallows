01f06e87b14c7b4a9a0449cfac0adb82e2d77bf4
Kelly Stoops <kstoops815@gmail.com>
2017-11-28 23:47:25 -0600

    google authentication and logout functions working

--- javascripts/controllers/AuthCtrl.js
+++ javascripts/controllers/AuthCtrl.js
@@ -1,5 +1,15 @@
 'use strict';
 
-app.controller("AuthCtrl", function () {
-    
-   });
+app.controller("AuthCtrl", function ($location, $rootScope, $scope, AuthService) {
+    $scope.authenticate = () => {
+    	AuthService.authenticateGoogle().then((result) => {
+    		$rootScope.uid = result.user.uid;
+    		console.log($rootScope.uid);
+    		$scope.$apply(() => {
+    			$location.url("/home");
+    		});
+    	}).catch((error) => {
+    		console.log("error in authenticateGoogle", error);
+    	});
+    };
+});
