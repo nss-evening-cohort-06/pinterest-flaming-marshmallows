31abed0885ccc497b8ce02c5df72df5550e6ada8
Dre mf Randaci <drerandaci@gmail.com>
2017-11-28 21:10:37 -0600

    boardctrl calls and location moves

--- javascripts/controllers/BoardCtrl.js
+++ javascripts/controllers/BoardCtrl.js
@@ -1,5 +1,32 @@
 'use strict';
 
-app.controller("BoardCtrl", function () {
-    
-   });
+app.controller("BoardCtrl", function ( $location, $rootScope, $scope, PinterestService ) {
+
+    const getBoards = () => {
+        PinterestService.SOMEFUNC().then((results) => {
+            $rootScope.uid = results.uid;
+            console.log("results in getFbBoards:", results);
+        }).catch((err) => {
+            console.log("err in getFbBoards:", err);
+        });
+    };
+    getBoards();
+
+    const getPins = () => {
+        PinterestService.SOMEFUNC().then((results) => {
+            console.log("results in getFbPins:", results);
+        }).catch((err) => {
+            console.log("err in getFbPins:", err);
+        });
+    };
+    getPins();
+
+    $scope.viewSingleBoard = (boardId) => {
+        $location.path(`/singleBoard/${boardId}`);
+    };
+
+    $scope.goToCreateBoard = () => {
+        $location.path(`/createBoard`);
+    };
+
+});
