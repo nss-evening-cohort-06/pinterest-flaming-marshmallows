61cfa9abf8160abea0794ed82860ef2307009828
Robbie Shock <rob.f.shock@gmail.com>
2017-11-28 21:35:12 -0600

    merging master into working branch

--- javascripts/controllers/PinCtrl.js
+++ javascripts/controllers/PinCtrl.js
@@ -1,5 +1,27 @@
 'use strict';
 
-app.controller("PinCtrl", function () {
-    
-   });
+app.controller("PinCtrl", function ($location, $rootScope, $scope, PinterestServices) {
+    $scope.pins = [];
+
+    const getPins = () => {
+        PinterestServices.getPins($rootScope.uid).then((results) => {
+            $scope.pins = results;
+        }).catch((err) => {
+            console.log("error in getPins", err);
+        });
+    };
+
+    getPins();
+
+    $scope.deletePin = (pinId) => {
+        PinterestServices.deletePin(pinId).then((results) => {
+            getPins();
+        }).catch((err) => {
+            console.log("error in deletePin", err);
+        });
+    };
+
+    $scope.pinDetail = (pinId) => {
+        $location.path(`/pin/${pinId}`);
+    };
+});
