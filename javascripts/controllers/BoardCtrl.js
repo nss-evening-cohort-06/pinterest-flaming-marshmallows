03ac390433277dc9b22cb158ddad4afa270b095f
Waamber <amberastuart@aol.com>
2017-11-29 14:26:44 -0600

    accessed fb data

--- javascripts/controllers/BoardCtrl.js
+++ javascripts/controllers/BoardCtrl.js
@@ -1,5 +1,15 @@
 'use strict';
 
-app.controller("BoardCtrl", function () {
-    
-   });
+app.controller("BoardCtrl", function ($rootScope, $scope, PinterestService) {
+
+  // const getBoards = () => {
+  //   PinterestService.getBoards($rootScope.uid).then((results) => {
+  //     $scope.boards = results;
+  //   }).catch((error) => {
+  //     console.log("Error in PinterestService", error);
+  //   });
+  // };
+
+  // getBoards();
+
+});
