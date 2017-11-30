bcae7661a8671d104be7028746024f7e4fb791b9
Waamber <amberastuart@aol.com>
2017-11-29 20:09:59 -0600

    edited ctrl route and added readme

--- javascripts/controllers/BoardCtrl.js
+++ javascripts/controllers/BoardCtrl.js
@@ -2,34 +2,34 @@
 
 app.controller("BoardCtrl", function ($location, $rootScope, $scope, PinterestService) {
 
-    const getBoards = () => {
-        PinterestService.getBoards($rootScope.uid).then((results) => {
-            $scope.boards = results;
-        }).catch((error) => {
-            console.log("Error in PinterestService.getboards()", error);
-        });
-    };
-    getBoards();
+  const getBoards = () => {
+    PinterestService.getBoards($rootScope.uid).then((results) => {
+      $scope.boards = results;
+    }).catch((error) => {
+      console.log("Error in PinterestService.getboards()", error);
+    });
+  };
+  getBoards();
 
-    const getPins = () => {
-        PinterestService.getPins().then((results) => {
-            $scope.pins = results;
-        }).catch((err) => {
-            console.log("err in getPins:", err);
-        });
-    };
-    getPins();
+  const getPins = () => {
+    PinterestService.getPins().then((results) => {
+      $scope.pins = results;
+    }).catch((err) => {
+      console.log("err in getPins:", err);
+    });
+  };
+  getPins();
 
   $scope.viewSingleBoard = (boardId) => {
     $location.path(`/singleBoard/${boardId}`);
   };
 
   $scope.goToCreateBoard = () => {
-    $location.path(`/singleBoard/create`);
+    $location.path(`/create`);
   };
 
-    $scope.deleteBoard = (boardId) => {
-        console.log("boardId:", boardId);
-    };
+  $scope.deleteBoard = (boardId) => {
+    console.log("boardId:", boardId);
+  };
 
 });
